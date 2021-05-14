eb87d0839755de48451ecf278806d6a1dfc4811f
Chia-Yi Chu <chiayi.joy.chu@gmail.com>
2021-05-13 21:39:14 -0700

    add incremental part

--- models/example/movie_gross_info_with_title.sql
+++ models/example/movie_gross_info_with_title.sql
@@ -2,3 +2,7 @@ SELECT i.movie_id, i.info_type_id, i.info, m.title, m.production_year
 FROM {{source('imdb', 'movie_info_sample')}} AS i INNER JOIN {{source('imdb', 'movie_sample')}} AS m
 ON i.movie_id = m.id
 WHERE i.info_type_id = 107
+
+{% if is_incremental() %}
+WHERE m.production_year > (SELECT MAX(production_year) FROM {{ this }})
+{% endif %}
